b23891d59768f2990f10fe66e2a67f700300f89b
Varun Patil <radialapps@gmail.com>
2018-08-27 17:24:41 +0530

    Show body roles in body admin

--- bodies/admin.py
+++ bodies/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from bodies.models import Body, BodyChildRelation
+from roles.models import BodyRole
 
-admin.site.register(Body)
+class RoleInline(admin.StackedInline):
+    """Show role names and links inline"""
+    model = BodyRole
+    fields = ('name',)
+    readonly_fields = ('name',)
+    template = 'role-inline.html'
+    show_change_link = True
+
+class BodyAdmin(admin.ModelAdmin):
+    """Admin model for Body model."""
+    inlines = [RoleInline]
+
+admin.site.register(Body, BodyAdmin)
 admin.site.register(BodyChildRelation)
